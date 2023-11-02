a59cff778df8f530d8265d3d1919183310a78594
Aurélien Kohler <aurelien@akdigital.fr>
2023-11-02 13:44:55 +0100

    test git workflow

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!defined('ABSPATH')) {
+if ( !defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
 
@@ -30,148 +30,49 @@ add_action( 'admin_enqueue_scripts', 'km_enqueue_custom_admin_style' );
 add_action( 'wp_ajax_wc_woocommerce_clear_cart_url', 'td_woocommerce_clear_cart_url' );
 add_action( 'wp_ajax_nopriv_wc_woocommerce_clear_cart_url', 'td_woocommerce_clear_cart_url' );
 
-function td_woocommerce_clear_cart_url()
-{
-    global $woocommerce;
+function td_woocommerce_clear_cart_url() {
+     global $woocommerce;
 
-    $returned = ['status' => 'error'];
+    $returned = array( 'status' => 'error' );
     $woocommerce->cart->empty_cart();
 
-    if ($woocommerce->cart->get_cart_contents_count() == 0) {
-        $returned = ['status' => 'success'];
+    if ( $woocommerce->cart->get_cart_contents_count() == 0 ) {
+        $returned = array( 'status' => 'success' );
     }
 
-    die(json_encode($returned));
+    die( json_encode( $returned ) );
 }
 
-add_action('wp_ajax_wc_before_popup', 'td_before_popup');
-add_action('wp_ajax_nopriv_wc_before_popup', 'td_before_popup');
+add_action( 'wp_ajax_wc_before_popup', 'td_before_popup' );
+add_action( 'wp_ajax_nopriv_wc_before_popup', 'td_before_popup' );
 
-function td_before_popup()
-{
-    global $woocommerce;
+function td_before_popup() {
+     global $woocommerce;
 
-    if (!isset($_POST['product_id']) || !isset($_POST['qty'])) {
-        die(json_encode(['status' => 'error']));
+    if ( !isset( $_POST['product_id'] ) || !isset( $_POST['qty'] ) ) {
+        die( json_encode( array( 'status' => 'error' ) ) );
     }
 
     // Vérifier si un "produit en vrac" est déjà dans le panier
     $isBulkProductInCart = false;
 
-    foreach ($woocommerce->cart->get_cart() as $cart_item_key => $cart_item) {
-        $_product = $cart_item['data'];
-        $addedProduct = wc_get_product($_POST['variation_id'] !== "" ? $_POST['variation_id'] : $_POST['product_id']);
+    foreach ( $woocommerce->cart->get_cart() as $cart_item_key => $cart_item ) {
+        $_product     = $cart_item['data'];
+        $addedProduct = wc_get_product( $_POST['variation_id'] !== '' ? $_POST['variation_id'] : $_POST['product_id'] );
 
-        if (strpos(strtoupper($_product->get_name()), "VRAC A LA TONNE") !== false &&
-            strpos(strtoupper($addedProduct->get_name()), "VRAC A LA TONNE") !== false) {
+        if ( strpos( strtoupper( $_product->get_name() ), 'VRAC A LA TONNE' ) !== false &&
+            strpos( strtoupper( $addedProduct->get_name() ), 'VRAC A LA TONNE' ) !== false ) {
             $isBulkProductInCart = true;
             break;
         }
     }
 
-    if ($isBulkProductInCart === true) {
+    if ( $isBulkProductInCart === true ) {
         // Afficher la popup pour indiquer que le produit en vrac est déjà dans le panier
-        die(json_encode([
-            'status' => 'vrac_popup',
-            'content' => "<style>
-#popup-content {
-  position: fixed;
-  width: 600px;
-  height: 400px;
-  top: 50%;
-  left: 50%;
-  margin-top: -100px;
-  margin-left: -250px;
-  background: white;
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  justify-content: center;
-  border-radius: 12px;
-}
-
-#close-popup {
-  position: absolute;
-  top: 12px;
-  right: 20px;
-}
-
-#popup-title {
-  font-weight: bold;
-  font-size: 22px;
-}
-
-#popup-content p {
-  text-align: center;
-  padding: 10px;
-  width: 370px;
-}
-
-#popup-content sub {
-  display: block;
-  height: 50px;
-  font-size: 13px;
-  width: 300px;
-  bottom: 0;
-  bottom: 0;
-  line-height: 20px;
-  line-height: 20px;
-  text-align: center;
-}
-
-#popup-btn button {
-  background-color: #bc9c64;
-  border-color: #bc9c64;
-  color: white;
-  font-weight: bold;
-  margin-top: 12px;
-  margin-bottom: 20px;
-}
-
-#popup-cart-link a {
-  font-size: 14px;
-  color: #bc9c64;
-  text-decoration: underline;
-  font-weight: 400;
-}
-
-#popup-shadow {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #0000001a;
-    z-index: 0;
-}
-
-#close-popup {
-    cursor: pointer;
-}
-</style><div id='popup-shadow'></div><div id='popup-content'><span id='close-popup' onclick='hidePopup()'>✖</span><p>Il est impossible d'ajouter des vracs de différents agrégats (gravier, galet, sable ou terre) dans le même camion. Merci de choisir des big bags si vous souhaitez différents agrégats ou choisir une option de livraison dans votre panier pour une livraison en vrac de l'ensemble des produits</p><div id='popup-btn'><button onclick='hidePopup()'>J'ai compris le message</button></div><div id='popup-cart-link'><a href='/panier'>Voir mon panier</a></div></div>"
-        ]));
-    }
-
-    $_product = wc_get_product($_POST['product_id']);
-
-    if (!is_object($_product)) {
-        die(json_encode(['status' => 'error']));
-    }
-
-    if (get_post_meta($_POST['product_id'], '_quantite_par_palette', true) !== false)  {
-        $nb_items_per_pallet = get_post_meta($_POST['product_id'], '_quantite_par_palette', true);
-
-        if ($nb_items_per_pallet) {
-            $nb_pallet_required = get_nb_palette_required($_POST['product_id'], $_POST['qty']);
-            $palette_id = get_palette_id((string)get_post_meta($_POST['product_id'], '_sku', true));
-
-            WC()->cart->add_to_cart($_POST['product_id'], $_POST['qty']);
-            // WC()->cart->add_to_cart($palette_id, $nb_pallet_required);
-
-            // Condition pour afficher la popup seulement si $nb_pallet_required est supérieur à 0
-            if ($nb_pallet_required >= 1) {
-                die(json_encode([
-                    'status' => 'palette_popup',
+        die(
+            json_encode(
+                array(
+                    'status'  => 'vrac_popup',
                     'content' => "<style>
 #popup-content {
   position: fixed;
@@ -247,19 +148,125 @@ function td_before_popup()
 #close-popup {
     cursor: pointer;
 }
-</style><div id='popup-shadow'></div><div id='popup-content'><span id='close-popup'>✖</span><p id='popup-title'>Produit ajouté au panier !</p><p>En plus de votre produit, <b>une/des palettes consignée(s)</b> ont été ajoutées automatiquement à votre panier.</p><sub>(28,80 € TTC la palette, remboursable à hauteur de 20,40 € TTC par palette).</sub><div id='popup-btn'><button>J'ai compris le message</button></div><div id='popup-cart-link'><a href='/panier'>Voir mon panier</a></div></div>"
-                ]));
+</style><div id='popup-shadow'></div><div id='popup-content'><span id='close-popup' onclick='hidePopup()'>✖</span><p>Il est impossible d'ajouter des vracs de différents agrégats (gravier, galet, sable ou terre) dans le même camion. Merci de choisir des big bags si vous souhaitez différents agrégats ou choisir une option de livraison dans votre panier pour une livraison en vrac de l'ensemble des produits</p><div id='popup-btn'><button onclick='hidePopup()'>J'ai compris le message</button></div><div id='popup-cart-link'><a href='/panier'>Voir mon panier</a></div></div>",
+                )
+            )
+        );
+    }
+
+    $_product = wc_get_product( $_POST['product_id'] );
+
+    if ( !is_object( $_product ) ) {
+        die( json_encode( array( 'status' => 'error' ) ) );
+    }
+
+    if ( get_post_meta( $_POST['product_id'], '_quantite_par_palette', true ) !== false ) {
+        $nb_items_per_pallet = get_post_meta( $_POST['product_id'], '_quantite_par_palette', true );
+
+        if ( $nb_items_per_pallet ) {
+            $nb_pallet_required = get_nb_palette_required( $_POST['product_id'], $_POST['qty'] );
+            $palette_id         = get_palette_id( (string) get_post_meta( $_POST['product_id'], '_sku', true ) );
+
+            WC()->cart->add_to_cart( $_POST['product_id'], $_POST['qty'] );
+            // WC()->cart->add_to_cart($palette_id, $nb_pallet_required);
+
+            // Condition pour afficher la popup seulement si $nb_pallet_required est supérieur à 0
+            if ( $nb_pallet_required >= 1 ) {
+                die(
+                    json_encode(
+                        array(
+                            'status'  => 'palette_popup',
+                            'content' => "<style>
+#popup-content {
+  position: fixed;
+  width: 600px;
+  height: 400px;
+  top: 50%;
+  left: 50%;
+  margin-top: -100px;
+  margin-left: -250px;
+  background: white;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 12px;
+}
+
+#close-popup {
+  position: absolute;
+  top: 12px;
+  right: 20px;
+}
+
+#popup-title {
+  font-weight: bold;
+  font-size: 22px;
+}
+
+#popup-content p {
+  text-align: center;
+  padding: 10px;
+  width: 370px;
+}
+
+#popup-content sub {
+  display: block;
+  height: 50px;
+  font-size: 13px;
+  width: 300px;
+  bottom: 0;
+  bottom: 0;
+  line-height: 20px;
+  line-height: 20px;
+  text-align: center;
+}
+
+#popup-btn button {
+  background-color: #bc9c64;
+  border-color: #bc9c64;
+  color: white;
+  font-weight: bold;
+  margin-top: 12px;
+  margin-bottom: 20px;
+}
+
+#popup-cart-link a {
+  font-size: 14px;
+  color: #bc9c64;
+  text-decoration: underline;
+  font-weight: 400;
+}
+
+#popup-shadow {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #0000001a;
+    z-index: 0;
+}
+
+#close-popup {
+    cursor: pointer;
+}
+</style><div id='popup-shadow'></div><div id='popup-content'><span id='close-popup'>✖</span><p id='popup-title'>Produit ajouté au panier !</p><p>En plus de votre produit, <b>une/des palettes consignée(s)</b> ont été ajoutées automatiquement à votre panier.</p><sub>(28,80 € TTC la palette, remboursable à hauteur de 20,40 € TTC par palette).</sub><div id='popup-btn'><button>J'ai compris le message</button></div><div id='popup-cart-link'><a href='/panier'>Voir mon panier</a></div></div>",
+                        )
+                    )
+                );
             }
         }
     }
 
-    die(json_encode(['status' => 'success']));
+    die( json_encode( array( 'status' => 'success' ) ) );
 }
 
 /**
  * Todo: TD | Fonctions après OAUTH, déclenchées sur la thank you page pour ajout d'un event au calendrier
  */
-/*add_action('woocommerce_thankyou', 'add_outlook_event');
+/*
+add_action('woocommerce_thankyou', 'add_outlook_event');
 
 function add_outlook_event($order_id)
 {
@@ -304,7 +311,8 @@ function add_outlook_event($order_id)
     }
 }*/
 
-/*function addEvent($client, $model)
+/*
+function addEvent($client, $model)
 {
     $startDate = new DateTime($model->date);
     $startDate->sub(new \DateInterval('PT1H'));
@@ -339,7 +347,8 @@ function add_outlook_event($order_id)
     return true;
 }*/
 
-/*function getClient()
+/*
+function getClient()
 {
     // Todo: put the access token here
     $accessToken = json_encode("");
@@ -372,7 +381,8 @@ function add_outlook_event($order_id)
 /**
  * Todo: TD | Fonctions OAUTH Outlook
  */
-/*function oauthMicrosoft(Request $request)
+/*
+function oauthMicrosoft(Request $request)
 {
     $client = getMicrosoftClient();
 
@@ -429,9 +439,11 @@ function add_outlook_event($order_id)
 
 function register_hello_world_widget( $widgets_manager ) {
 
-    require_once( __DIR__ . '/widgets/account.php' );
+    require_once __DIR__ . '/widgets/account.php';
 
     $widgets_manager->register( new \Account_Widget() );
 
 }
 add_action( 'elementor/widgets/register', 'register_hello_world_widget' );
+
+/* Test git workflow */
